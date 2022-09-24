a6d2fad5aaced342486dd2832e15412b62907284
Raymond <101374892+raymonable@users.noreply.github.com>
2022-09-24 00:17:52 -0400

    sample might work idk can't test atm

---
+++ KeyframeEmulator/sample.lua
@@ -0,0 +1,23 @@
+--[[
+
+KeyframeEmulator Sample
+
+Additional Notes:
+ [1]: You don't have to load this from a URL. You can plug the module directly, but if you do, make sure to either wrap it in a function or remove the "return" statement at the beginning.
+
+--]]
+
+local Success, SerializedAnimation = pcall(function()
+      return game:GetService('HttpService'):GetAsync('your.animation.url.here') -- Check NOTES 1
+end)
+if not Success then print('Failed to download animation') end
+SerializedAnimation = loadstring(SerializedAnimation)()
+
+local Success, KeyframeEmulator = pcall(function()
+      return game:GetService('HttpService'):GetAsync('https://raw.githubusercontent.com/QTpiV2/qtpi/main/KeyframeEmulator/module.lua')
+end)
+if not Success then print('Failed to download emulator') end
+KeyframeEmulator = loadstring(KeyframeEmulator)()
+local Animation = KeyframeEmulator:LoadAnimation(script.Parent, Serialized)
+
+Animation.Play() -- Play the animation.
